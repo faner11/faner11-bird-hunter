de8c248e58096cef546944cad2708935d0c00766
Bob <fhlqwe@gmail.com>
2024-09-30 17:25:38 +0800

    update

---
+++ bird-hunter.meta.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         专业捕鸟人
+// @namespace    http://tampermonkey.net/5GVBKlRpx98iVhG/mu2XMB
+// @version      2024-09-30
+// @author       Bob
+// @description  合作加vx
+// @icon         https://github.com/faner11/faner11-bird-hunter/blob/main/IMG_5007.jpeg?raw=true
+// @downloadURL  https://github.com/faner11/faner11-bird-hunter/raw/main/bird-hunter.user.js
+// @updateURL    https://github.com/faner11/faner11-bird-hunter/raw/main/bird-hunter.meta.js
+// @match        https://arcteryx.com/*
+// @match        https://outlet.arcteryx.com/*
+// @match        http://localhost:*/*
+// @match        https://*.ngrok-free.app/*
+// @connect      mixpanel.com
+// @grant        GM_addValueChangeListener
+// @grant        GM_deleteValue
+// @grant        GM_getValue
+// @grant        GM_openInTab
+// @grant        GM_removeValueChangeListener
+// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+// @run-at       document-start
+// ==/UserScript==
