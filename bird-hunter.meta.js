75ea04855dfb292936b9f1460141c4096d620538
fh <fhlqwe@gmail.com>
2024-10-09 13:25:43 +0800

    update

--- bird-hunter.meta.js
+++ bird-hunter.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         专业捕鸟人
 // @namespace    http://tampermonkey.net/5GVBKlRpx98iVhG/mu2XMB
-// @version      2024-10-09
+// @version      20241008
 // @author       Bob
 // @description  合作加vx
 // @icon         https://github.com/faner11/faner11-bird-hunter/blob/main/IMG_5007.jpeg?raw=true
