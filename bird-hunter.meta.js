ca83678b5bb8ab0e3f26f629c9e3c2f865391ce5
fh <fhlqwe@gmail.com>
2024-10-20 11:11:04 +0800

    update

--- bird-hunter.meta.js
+++ bird-hunter.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         专业捕鸟人
 // @namespace    http://tampermonkey.net/5GVBKlRpx98iVhG/mu2XMB
-// @version      20241009
+// @version      20241020
 // @author       Bob
 // @description  合作加vx
 // @icon         https://github.com/faner11/faner11-bird-hunter/blob/main/IMG_5007.jpeg?raw=true
@@ -12,9 +12,13 @@
 // @match        http://localhost:*/*
 // @match        https://*.ngrok-free.app/*
 // @match        https://*.goodhunter.cc/*
+// @resource     vconsole  https://cdn.jsdelivr.net/npm/vconsole@latest/dist/vconsole.min.js
 // @connect      mixpanel.com
+// @grant        GM.info
 // @grant        GM_addValueChangeListener
+// @grant        GM_cookie
 // @grant        GM_deleteValue
+// @grant        GM_getResourceText
 // @grant        GM_getValue
 // @grant        GM_openInTab
 // @grant        GM_removeValueChangeListener
